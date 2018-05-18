bf8deee07d50254403214cb82c863190de807055
wyattlindsey <wyatt@spatially.com>
2018-05-17 22:46:34 -0700

    add three components

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,16 +1,16 @@
 import * as React from 'react'
-import Store from 'store'
-import { setPending } from 'actions/loading'
+
+const wrapperStyle = {
+  fontFamily: 'Roboto',
+  fontSize: '3rem',
+  textAlign: 'center',
+}
 
 class Main extends React.PureComponent {
-  handleClick = () => {
-    Store.dispatch(setPending('One potato'))
-  }
-
   render() {
     return (
-      <div>
-        <button onClick={this.handleClick}>Set Pending</button>
+      <div style={wrapperStyle}>
+        <div>React Redux ThreeJS Lite</div>
       </div>
     )
   }
